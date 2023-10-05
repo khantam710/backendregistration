d30d380a7b7150f0fe3bd0fff2520ed4d9e3fe41
khantam710 <134850822+khantam710@users.noreply.github.com>
2023-10-05 13:33:49 +0530

    changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 4000
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: [process.env.REACT_URL1],
   optionsSuccessStatus: 200,
 };
 
