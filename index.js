694e9dfcf06756c0e94355725d8f531694910580
khantam710 <134850822+khantam710@users.noreply.github.com>
2023-10-05 13:57:57 +0530

    changes

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
 // console.log("Hello")
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 import cors from "cors";
-import userRouter from './routers/user.router';
+import userRouter from "./routers/user.router";
 import fs from "fs";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
-const corsOptions = {
-  origin: [process.env.REACT_URL1],
-  optionsSuccessStatus: 200,
-};
+// const corsOptions = {
+//   // origin: [process.env.REACT_URL1],
+//   optionsSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 
 //create application/json parser
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 //create application/x-ww-form-urlencoded parser
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(port, () =>{
-    console.log(`Example app listening on port ${port}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
-  
-const uploadDir = './uploads';
+
+const uploadDir = "./uploads";
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
 
-
-process.env.MONGO_URL = 'mongodb+srv://khantam710:Tam123@cluster0.cidj3bf.mongodb.net/registration';
+process.env.MONGO_URL =
+  "mongodb+srv://khantam710:Tam123@cluster0.cidj3bf.mongodb.net/registration";
 
 console.log(process.env.MONGO_URL);
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log(`Connected to Server!`))
-  .catch(error => console.error(`Error connecting to database: ${error.message}`));
+  .catch((error) =>
+    console.error(`Error connecting to database: ${error.message}`)
+  );
 
-
-app.use('/registration/user', userRouter)
+app.use("/registration/user", userRouter);
